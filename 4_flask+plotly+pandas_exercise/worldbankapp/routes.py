43e948ae3630f6f7e4599922b0fd7f52fde31bee
kalibrahim <bowaleed500@gmail.com>
2019-07-17 12:48:49 +0300

    Added ex4

--- 4_flask+plotly+pandas_exercise/worldbankapp/routes.py
+++ 4_flask+plotly+pandas_exercise/worldbankapp/routes.py
@@ -1,9 +1,7 @@
 from worldbankapp import app
-import json
-import plotly
+import json, plotly
 from flask import render_template
 from wrangling_scripts.wrangle_data import return_figures
-
 
 @app.route('/')
 @app.route('/index')
