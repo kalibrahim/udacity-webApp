87ba51f06de3dbdf83a4762ff9940e39eb68eeb5
kalibrahim <bowaleed500@gmail.com>
2019-07-17 09:43:07 +0300

    Ex jinja and flask

--- Flask/worldbankapp/routes.py
+++ Flask/worldbankapp/routes.py
@@ -1,12 +1,17 @@
 from worldbankapp import app
 from flask import render_template
+import pandas as pd
+from wrangling_scripts.wrangling import data_wrangling
+
+import plotly.graph_objs as go 
+import plotly, json
+
+data = data_wrangling()
+
+print(data)
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
-
-@app.route('/project_one')
-def project():
-    return render_template('project_one.html')
+    return render_template('index.html', data_set = data)
